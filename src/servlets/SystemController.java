9450fd334eefd914b6a676eeadcb8bd94f55c00b
SnapMickey <jared_rivera@dlsu.edu.ph>
2017-10-19 20:20:28 +0800

    Add files via upload

--- src/servlets/SystemController.java
+++ src/servlets/SystemController.java
@@ -7,6 +7,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.google.gson.Gson;
 
 import beans.User;
 import services.SystemService;
@@ -14,7 +15,7 @@ import services.SystemService;
 /**
  * Servlet implementation class SystemController
  */
-@WebServlet(urlPatterns = {"/login", "logout"})
+@WebServlet(urlPatterns = {"/login", "/logout"})
 @MultipartConfig
 public class SystemController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -47,39 +48,56 @@ public class SystemController extends HttpServlet {
 		//response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
-	private void Logout(HttpServletRequest request, HttpServletResponse response) {
+	private void Logout(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		request.getSession().invalidate();
+		request.getRequestDispatcher("index.html").forward(request, response);
 		//redirect to login page
 	}
 
-	private void Login(HttpServletRequest request, HttpServletResponse response) throws IOException {
+	private void Login(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException, NullPointerException{
 		// TODO Auto-generated method stub
-
-		int id = Integer.parseInt(request.getParameter("id"));
-		String password = request.getParameter("password");
+		
+		System.out.print("HEK");
+		
+		int id = -1;
+		String password = "";
+		String link = "index.html?error=yes";
+		
+		try {
+			id = Integer.parseInt(request.getParameter("id"));
+			
+			password = request.getParameter("password");
+			
+			System.out.println("ID: " + id);
+			System.out.println("Password: " + password);
 		
 		User user = SystemService.getUser(id);
 		
-		if(user == null) {
-			//send invalid id message 
-			response.sendRedirect("");
-		}
-		else if(!user.getPassword().equals(password)) {
-			//send invalid password message
-			response.sendRedirect("");
+		if(user == null || !user.getPassword().equals(password)) {
+			System.out.println("USER INVALID");
 		}
 		else {
+			System.out.println("USER VALID");
 			request.getSession().setAttribute("id", id);
 			
 			if(user.getPosition().equals("admin")) {
-				//redirect to admin homepage
+				link = "admin_front_page.html";
 			}
 			else {
-				//redirect to user homepage
+				link = "user_front_page.html";
 			}
 		}
-			
+		
+		}
+		catch(Exception e) {
+			System.out.println(e);
+			e.printStackTrace();
+		}
+		
+		System.out.println("END");
+		//request.getRequestDispatcher(link).forward(request,response);
+		response.sendRedirect(link);
 	}
 
 	/**
--- src/test/test.java
+++ src/test/test.java
@@ -5,6 +5,6 @@ import services.SystemService;
 public class test {
 
 	public static void main(String args[]) {
-		System.out.println(SystemService.getUser(11012344));
+		System.out.println(SystemService.getUser(11012344).getPosition());
 	}
 }
